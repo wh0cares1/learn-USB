3185ffb3a649cdade597878573a3aea9fd2d7cb7
wh0cares1 <95743065+wh0cares1@users.noreply.github.com>
2024-04-07 13:55:59 +0700

    Update usbd_driver.c

--- Src/usbd_driver.c
+++ Src/usbd_driver.c
@@ -20,4 +20,42 @@ static void initialize_gpio_pins()
 	);
 }
 
+static void initialize_core()
+{
+	// Enables the clock for USB core.
+	SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_OTGHSEN);
+
+	// Configures the USB core to run in device mode, and to use the embedded full-speed PHY.
+	MODIFY_REG(USB_OTG_HS->GUSBCFG,
+		USB_OTG_GUSBCFG_FDMOD | USB_OTG_GUSBCFG_PHYSEL | USB_OTG_GUSBCFG_TRDT,
+		USB_OTG_GUSBCFG_FDMOD | USB_OTG_GUSBCFG_PHYSEL | _VAL2FLD(USB_OTG_GUSBCFG_TRDT, 0x09)
+	);
+
+	// Configures the device to run in full speed mode.
+	MODIFY_REG(USB_OTG_HS_DEVICE->DCFG,
+		USB_OTG_DCFG_DSPD,
+		_VAL2FLD(USB_OTG_DCFG_DSPD, 0x03)
+	);
+
+	// Enables VBUS sensing device.
+	SET_BIT(USB_OTG_HS->GCCFG, USB_OTG_GCCFG_VBUSBSEN);
+
+	// Unmasks the main USB core interrupts.
+	SET_BIT(USB_OTG_HS->GINTMSK,
+		USB_OTG_GINTMSK_USBRST | USB_OTG_GINTMSK_ENUMDNEM | USB_OTG_GINTMSK_SOFM |
+		USB_OTG_GINTMSK_USBSUSPM | USB_OTG_GINTMSK_WUIM | USB_OTG_GINTMSK_IEPINT |
+		USB_OTG_GINTSTS_OEPINT | USB_OTG_GINTMSK_RXFLVLM
+	);
+
+	// Clears all pending core interrupts.
+	WRITE_REG(USB_OTG_HS->GINTSTS, 0xFFFFFFFF);
+
+	// Unmasks USB global interrupt.
+	SET_BIT(USB_OTG_HS->GAHBCFG, USB_OTG_GAHBCFG_GINT);
+
+	// Unmasks transfer completed interrupts for all endpoints.
+	SET_BIT(USB_OTG_HS_DEVICE->DOEPMSK, USB_OTG_DOEPMSK_XFRCM);
+	SET_BIT(USB_OTG_HS_DEVICE->DIEPMSK, USB_OTG_DIEPMSK_XFRCM);
+}
+
 
